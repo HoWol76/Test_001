4de6760cf0e2f4c2776b6a42417a09d8cdd8cc97
Holger Wolff <holger.wolff@monash.edu>
2015-08-11 12:13:20 +1000

    added syntax error to main.f90

--- main.f90
+++ main.f90
@@ -1,4 +1,9 @@
 program main
     implicit none
+    integer :: i
     print *, "Hello World"
+
+    do i = 1, 5
+        write(*, '(A, I2, A)') "In iteration ", i, " of the loop."
+
 end program main
